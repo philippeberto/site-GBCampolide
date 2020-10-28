078ee790f31f16511b5d6f30fdfee13c4c9a6ddd
Philippe <philippe.bjj@gmail.com>
2020-10-28 07:19:01 +0000

    correção de variavies de ambiente

--- pages/api/add-contact.js
+++ pages/api/add-contact.js
@@ -1,12 +1,22 @@
 import { GoogleSpreadsheet } from 'google-spreadsheet'
 import moment from 'moment'
-import credentials from '../../planilha/credentials.json'
 
 const doc = new GoogleSpreadsheet(process.env.SHEET_DOC_ID)
 
 export default async (req, res) => {
   try {
-    await doc.useServiceAccountAuth(credentials)
+    await doc.useServiceAccountAuth({
+      type: process.env.SHEET_TYPE,
+      project_id: process.env.SHEET_PROJECT_ID,
+      private_key_id: process.env.SHEET_PRIVATE_KEY_ID,
+      private_key: process.env.SHEET_PRIVATE_KEY,
+      client_email: process.env.SHEET_CLIENT_EMAIL,
+      client_id: process.env.SHEET_CLIENT_ID,
+      auth_uri: process.env.SHEET_AUTH_URI,
+      token_uri: process.env.SHEET_TOKEN_URI,
+      auth_provider_x509_cert_url: process.env.SHEET_AUTH_PROVIDER_X509_CERT_URL,
+      client_x509_cert_url: process.env.SHEET_CLIENT_X509_CERT_URL
+    })
     await doc.loadInfo()
     const sheet = doc.sheetsByIndex[1]
     const data = JSON.parse(req.body)
