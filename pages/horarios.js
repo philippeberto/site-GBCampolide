2e9daa4dcca388ccf598180bca638a1d955243cd
philippeberto <philippeberto@outlook.com>
2021-05-10 20:49:04 +0100

    new schedule added

--- pages/horarios.js
+++ pages/horarios.js
@@ -9,11 +9,11 @@ const Schedule = () => {
       <div>
         <img
           className="img-schedule"
-          src="img/Horários_2020-21.png"
+          src="img/Horarios_2020-21.jpeg"
           alt="Imagem dos Horários"
         ></img>
-        <p className='p-schedule'>As aulas podem ser suspensas por algum motivo excepcional, 
-        devidamente comunicado a todos os alunos antecipadamente. Para aulas experimentais é 
+        <p className='p-schedule'>As aulas podem ser suspensas por algum motivo excepcional,
+        devidamente comunicado a todos os alunos antecipadamente. Para aulas experimentais é
         necessário marcação prévia.</p>
       </div>
       <Contact />
