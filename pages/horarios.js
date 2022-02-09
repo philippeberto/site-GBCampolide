ec8ccd780d4a8c1f9be4b0c5a5d89ec8cf9c9e4f
philippeberto <philippeberto@outlook.com>
2022-02-09 10:50:33 +0000

    change schedule image

--- pages/horarios.js
+++ pages/horarios.js
@@ -1,6 +1,6 @@
-import Header from "../components/header"
-import Contact from "../components/contact"
-import Footer from "../components/footer"
+import Header from "../components/header";
+import Contact from "../components/contact";
+import Footer from "../components/footer";
 
 const Schedule = () => {
   return (
@@ -9,18 +9,20 @@ const Schedule = () => {
       <div>
         <img
           className="img-schedule"
-          src="img/Horarios_2020-21.jpeg"
+          src="img/horarios.jpeg"
           alt="Imagem dos Horários"
         ></img>
-        <p className='p-schedule'>As aulas podem ser suspensas por algum motivo excepcional,
-        devidamente comunicado a todos os alunos antecipadamente. Para aulas experimentais é
-        necessário marcação prévia.</p>
+        <p className="p-schedule">
+          As aulas podem ser suspensas por algum motivo excepcional, devidamente
+          comunicado a todos os alunos antecipadamente. Para aulas experimentais
+          é necessário marcação prévia.
+        </p>
       </div>
       <Contact />
 
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Schedule
+export default Schedule;
